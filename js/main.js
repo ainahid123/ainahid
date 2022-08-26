1df134016222995ad6aa5351ed7d5e8fcab0dee7
AI NAHID <mdarifulislamnahid125108@gmail.com>
2022-08-26 22:51:30 +0600

    fix contact form

--- js/main.js
+++ js/main.js
@@ -8,3 +8,32 @@ document.getElementById("more").addEventListener("click", function(){
         showMore = true;
     }
 })
+
+
+
+
+document.getElementById("sendmail").addEventListener("click", function(){
+    const from_name = document.getElementById("fullname");
+    const email_id = document.getElementById("email_id");
+    const message = document.getElementById("message");
+
+    const from_name_value = from_name.value;
+    const email_id_value = email_id.value;
+    const message_value = message.value;
+
+    let params = {
+        from_name : from_name_value,
+        email_id : email_id_value,
+        message : message_value
+    }
+
+    console.log(params);
+    emailjs.init("rjluBq6Y9IfutnjJ1");
+    emailjs.send("service_nfb7ncq", "template_4vb1aoh", params).then(function(res){
+        if(res.status === 200){
+            alert("Send Success")
+        }else{
+            alert("Please send again")
+        }
+    })
+});
